f61860389790bc344e285043cc4de18f7e00ca23
Shayxon <shayxon1@gmail.com>
2024-02-29 18:09:27 +0500

    add Order and OrderItem models to admin

--- orders/models.py
+++ orders/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from shop.models import Product
 
 class Order(models.Model):
-    name = models.CharField(max_length=50)
-    surename = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     email = models.EmailField()
     address = models.CharField(max_length=250)
     postal_code = models.CharField(max_length=20)
